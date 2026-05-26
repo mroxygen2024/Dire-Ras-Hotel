44f8d6c4f1333a2eb9e9801420dd22806a52a765
fuad sano <fuad.sano@astu.edu.et>
2026-05-26 14:20:52 +0300

    feat: add ReviewsSection component and integrate it into the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import BookingCard from '../components/BookingCard';
 import FeaturesSection from '../components/FeaturesSection';
 import RoomsSection from '../components/RoomsSection';
 import WhyStaySection from '../components/WhyStaySection';
+import ReviewsSection from '../components/ReviewsSection';
 import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
 import WhatsAppFloatingButton from '../components/WhatsAppFloatingButton';
@@ -96,7 +97,10 @@ export default function Home() {
         {/* 6. Why Stay With Us Section */}
         <WhyStaySection loading={loading} />
 
-        {/* 7. Contact & Location Section */}
+        {/* 7. Guest Reviews Section */}
+        <ReviewsSection loading={loading} />
+
+        {/* 8. Contact & Location Section */}
         <ContactSection loading={loading} />
       </main>
 
